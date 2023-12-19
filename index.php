c6429aab953e00bc7e5a1d7bf516dc6f583fc73c
Lennis <128396922+lennis-dev@users.noreply.github.com>
2023-12-19 10:46:46 +0100

    Update page titles in index.php

--- index.php
+++ index.php
@@ -6,23 +6,22 @@ require_once __DIR__ . "/core/UI.php";
 require_once __DIR__ . "/core/Config.php";
 
 use LennisDev\XAuth\UI;
-use LennisDev\XAuth\Config;
 
 if ($_SERVER["REQUEST_URI"] == "/register") {
     $ui = new UI([
-        "title" => "XAuth",
+        "title" => "XAuth - Register",
         "description" => "XAuth is a simple authentication system for your website."
     ], UI::$REGISTER_PAGE);
     exit;
 } else if ($_SERVER["REQUEST_URI"] == "/authorize") {
     $ui = new UI([
-        "title" => "XAuth",
+        "title" => "XAuth - Authorize",
         "description" => "XAuth is a simple authentication system for your website."
     ], UI::$AUTHORIZE_PAGE);
     exit;
 } else if ($_SERVER["REQUEST_URI"] == "/login") {
     $ui = new UI([
-        "title" => "XAuth",
+        "title" => "XAuth - Login",
         "description" => "XAuth is a simple authentication system for your website."
     ], UI::$LOGIN_PAGE);
     exit;
