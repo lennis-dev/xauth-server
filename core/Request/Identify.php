fe7201379fb9c0a30d37d1a02f316ab0e27beb1b
Lennis <128396922+lennis-dev@users.noreply.github.com>
2024-02-10 11:35:06 +0100

    Add token expiration and verification checks in Identify::exec() method

--- core/Request/Identify.php
+++ core/Request/Identify.php
@@ -13,9 +13,16 @@ class Identify
     public static function exec(\LennisDev\XAuth\Request $obj)
     {
         try {
-            $obj->checkAuth();
             if (!$obj->getToken()->hasScope("identify")) {
                 $obj->error("Token does not have identify scope");
+                return;
+            }
+            if (!$obj->getToken()->checkExpire()) {
+                $obj->error("Token has expired");
+                return;
+            }
+            if (!$obj->getToken()->checkVerify()) {
+                $obj->error("Token is invalid");
                 return;
             }
             $obj->return(true, [
