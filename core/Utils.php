c81d2be6de5c9ab0dd27f993e37068600db06e15
Lennis <128396922+lennis-dev@users.noreply.github.com>
2024-03-17 19:29:54 +0100

    Refactor Utils class to use str_starts_with() function
    This commit refactors the Utils class to use the str_starts_with() function instead of strpos() for improved readability and maintainability.

--- core/Utils.php
+++ core/Utils.php
@@ -14,7 +14,7 @@ class Utils
         $realdpath = realpath($path);
         if ($realdpath !== false) {
             foreach ($whitelistDirs as $dir) {
-                if (strpos($realdpath, $dir) === 0 && file_exists($path)) {
+                if (str_starts_with($realdpath,   $dir) && file_exists($path)) {
                     return json_decode(file_get_contents($path), true);
                 }
             }
@@ -28,7 +28,7 @@ class Utils
         $realdpath = realpath($path);
         if ($realdpath !== false) {
             foreach ($whitelistDirs as $dir) {
-                if (strpos($realdpath, $dir) === 0) {
+                if (str_starts_with($realdpath, $dir)) {
                     return file_put_contents($path, json_encode($data, JSON_PRETTY_PRINT)) !== false;
                 }
             }
