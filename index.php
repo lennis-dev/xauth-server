85f4b85331bcfe35bf2a21926f0226397f286c2e
Lennis <128396922+lennis-dev@users.noreply.github.com>
2024-04-19 12:40:33 +0200

    Update page titles and descriptions in index.php

--- index.php
+++ index.php
@@ -9,20 +9,20 @@ use LennisDev\XAuth\UI;
 
 if ($_SERVER["REQUEST_URI"] == "/register") {
     $ui = new UI([
-        "title" => "XAuth - Register",
-        "description" => "XAuth is a simple authentication system for your website."
+        "title" => "xAuth - Register",
+        "description" => "xAuth is a simple authentication system for your website."
     ], UI::$REGISTER_PAGE);
     exit;
 } else if ($_SERVER["REQUEST_URI"] == "/authorize") {
     $ui = new UI([
-        "title" => "XAuth - Authorize",
-        "description" => "XAuth is a simple authentication system for your website."
+        "title" => "xAuth - Authorize",
+        "description" => "xAuth is a simple authentication system for your website."
     ], UI::$AUTHORIZE_PAGE);
     exit;
 } else if ($_SERVER["REQUEST_URI"] == "/login") {
     $ui = new UI([
-        "title" => "XAuth - Login",
-        "description" => "XAuth is a simple authentication system for your website."
+        "title" => "xAuth - Login",
+        "description" => "xAuth is a simple authentication system for your website."
     ], UI::$LOGIN_PAGE);
     exit;
 }
